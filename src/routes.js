e6e038a66c20e24db26ff9524e07dc08ce47ae49
WOJTOP_PC\48690 <wojtop@interia.pl>
2024-07-05 15:57:04 +0200

    start

--- src/routes.js
+++ src/routes.js
@@ -25,11 +25,11 @@ import UserProfile from "views/UserProfile.js";
 
 var routes = [
   {
-    path: "/",
+    path: "/dashboard",
     name: "Statystyki Drużyny",
     icon: "tim-icons icon-chart-pie-36",
     component: <Dashboard />,
-    layout: "/VodkaVolleys",
+    layout: "/admin",
   },
   // {
   //   path: "/icons",
@@ -55,18 +55,18 @@ var routes = [
   //   layout: "/admin",
   // },
   {
-    path: "/",
+    path: "/main_table",
     name: "tabela strzelców",
     icon: "tim-icons icon-puzzle-10",
     component: <TableList />,
-    layout: "/VodkaVolleys",
+    layout: "/admin",
   },
   {
-    path: "/",
+    path: "/players",
     name: "zawodnicy",
     icon: "tim-icons icon-single-02",
     component: <Players />,
-    layout: "/VodkaVolleys",
+    layout: "/admin",
   },
   // {
   //   path: "/typography",
