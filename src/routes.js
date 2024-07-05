53628a9176a3ea2cc9f309be1232dbc1f7e09db3
WOJTOP_PC\48690 <wojtop@interia.pl>
2024-07-05 15:37:33 +0200

    reverse changes

--- src/routes.js
+++ src/routes.js
@@ -29,7 +29,7 @@ var routes = [
     name: "Statystyki Drużyny",
     icon: "tim-icons icon-chart-pie-36",
     component: <Dashboard />,
-    layout: "/admin",
+    layout: "/VodkaVolleys",
   },
   // {
   //   path: "/icons",
@@ -59,14 +59,14 @@ var routes = [
     name: "tabela strzelców",
     icon: "tim-icons icon-puzzle-10",
     component: <TableList />,
-    layout: "/admin",
+    layout: "/VodkaVolleys",
   },
   {
     path: "/players",
     name: "zawodnicy",
     icon: "tim-icons icon-single-02",
     component: <Players />,
-    layout: "/admin",
+    layout: "/VodkaVolleys",
   },
   // {
   //   path: "/typography",
